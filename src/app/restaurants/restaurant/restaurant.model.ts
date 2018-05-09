48834a3a3922bc745bac79d34f5395a13edea825
Marcos S. Cardoso <markard@gmail.com>
2018-05-09 13:04:52 -0400

    Component restaurante

---
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,17 @@
+export interface Restaurant {
+    id: string,
+    name: string,
+    category: string,
+    deliveryEstimate: string,
+    rating: number,
+    imagePath: string
+
+  /*  "id"   : "bread-bakery",
+    "name" : "Bread & Bakery",
+    "category" : "Bakery",
+    "deliveryEstimate" : "25m",
+    "rating": 4.9,
+    "imagePath": "assets/img/restaurants/breadbakery.png",
+    "about" : "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
+    "hours" : "Funciona de segunda a sexta, de 8h as 23h"*/
+}
