243cbf8512573da622d39cd1bdb6e59d2c06cdd8
Marcos S. Cardoso <markard@gmail.com>
2018-05-16 12:51:53 -0400

    Component restaurante

---
+++ src/app/restaurant-detail/shpping-cart/shopping-cart.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-shpping-cart',
+  templateUrl: './shpping-cart.component.html'
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import {Restaurant} from "./restaurant/restaurant.model";
 import { MEAT_API } from "../app.api";
+import 'rxjs/add/operator/catch'
+import {ErrorHandler} from "../app.error-handler";
 
 @Injectable()
 export class RestaurantsService {
@@ -33,5 +35,6 @@ export class RestaurantsService {
        // return this.rests
         return this.http.get( `${MEAT_API}/restaurants` )
             .map( response => response.json() )
+            .catch( ErrorHandler.handlerError )
     }
 }
