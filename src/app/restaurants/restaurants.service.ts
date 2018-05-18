241cf34dceab2ab9eb375da730f059899da7a062
Marcos S. Cardoso <markard@gmail.com>
2018-05-18 12:27:04 -0400

    Reviews restaurant

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -42,6 +42,12 @@ export class RestaurantsService {
         return this.http.get( `${MEAT_API}/restaurants/${id}` )
             .map( response => response.json() )
             .catch( ErrorHandler.handlerError )
+    }
+
+    reviewsOfRestaurant( id: string ): Observable<any>{
+        return this.http.get( `${MEAT_API}/restaurants/${id}/reviews` )
+            .map( response => response.json() )
+            .catch( ErrorHandler.handlerError )
 
     }
 }
