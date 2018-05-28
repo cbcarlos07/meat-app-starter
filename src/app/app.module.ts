e214ec2ee8f16ac09d0d3714489f5d559fd61603
Marcos S. Cardoso <markard@gmail.com>
2018-05-28 12:39:39 -0400

    Criando componente usando ControlValueAccessor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import {ShoppingCartService} from "./restaurant-detail/shpping-cart/shopping-car
 import { OrderComponent } from './order/order.component';
 import { FormsModule } from "@angular/forms";
 import {InputComponent} from "./shared/input/input.component";
+import {RadioComponent} from "./shared/radio/radio.component";
+
 
 
 @NgModule({
@@ -37,7 +39,8 @@ import {InputComponent} from "./shared/input/input.component";
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
