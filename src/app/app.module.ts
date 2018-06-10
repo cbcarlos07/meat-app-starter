8037a692276abc97e1d7016eb3973239fa127400
carlos <cbcarlos07@gmail.com>
2018-06-10 14:50:58 -0400

    Modulos Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { ROUTES } from "./app.routes";
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
 import { RestaurantsService } from "./restaurants/restaurants.service";
@@ -18,15 +17,9 @@ import { ShoppingCartComponent } from "./restaurant-detail/shpping-cart/shopping
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import {ShoppingCartService} from "./restaurant-detail/shpping-cart/shopping-cart.service";
-import { OrderComponent } from './order/order.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {InputComponent} from "./shared/input/input.component";
-import {RadioComponent} from "./shared/radio/radio.component";
-import {OrderItemsComponent} from "./order/order-items/order-items.component";
 import {OrderService} from "./order/order.service";
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSumaryComponent } from './order/order-sumary/order-sumary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+import {SharedModule} from "./shared/shared.module";
 
 
 
@@ -36,7 +29,6 @@ import { RatingComponent } from './shared/rating/rating.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -44,19 +36,12 @@ import { RatingComponent } from './shared/rating/rating.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
-    OrderSumaryComponent,
-    RatingComponent
+    OrderSumaryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
     RouterModule.forRoot( ROUTES )
   ],
   providers: [ RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'} ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import {Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
 import {RestaurantsComponent} from "./restaurants/restaurants.component";
 import {RestaurantDetailComponent} from "./restaurant-detail/restaurant-detail.component";
 import {MenuComponent} from "./restaurant-detail/menu/menu.component";
 import {ReviewsComponent} from "./restaurant-detail/reviews/reviews.component";
-import {OrderComponent} from "./order/order.component";
 import {OrderSumaryComponent} from "./order/order-sumary/order-sumary.component";
+
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
@@ -18,8 +17,8 @@ export const ROUTES: Routes = [
            { path: 'reviews', component: ReviewsComponent }
        ]
     },
-    { path: 'about', component: AboutComponent},
+    { path: 'about', loadChildren: './about/about.module#AboutModule'},
     { path: 'order-sumary', component: OrderSumaryComponent},
-    { path: 'order', component: OrderComponent}
+    { path: 'order', loadChildren: './order/order.module#OrderModule'}
 
 ]
