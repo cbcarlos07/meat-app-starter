a8f2987bbea6dde5318c9d2ac98b59dc2abe4f24
carlos <cbcarlos07@gmail.com>
2018-07-02 18:36:05 -0400

    Aula 115 seção 14 Middlware de autorização

--- backend/users.ts
+++ backend/users.ts
@@ -6,7 +6,7 @@ export class User {
         return another !== undefined && another.email === this.email && another.password === this.password
     }
 }
-export const users  = {
+export const users: {[key:string]: User}  = {
     "carlos@mail.com": new User('carlos@mail.com','Carlos','123'),
     "bruno@mail.com": new User('bruno@mail.com','Bruno','321'),
 }
